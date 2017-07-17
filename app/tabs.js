9f4d6625efbbad5615d34bd7aa35a6676a32b17b
Benjamin Tam <ben@teamtam.com>
2017-07-17 16:16:14 +1000

    Icons for iOS

--- app/tabs.js
+++ app/tabs.js
@@ -8,24 +8,28 @@ import { TabNavigator } from 'react-navigation';
 import Home from "./home";
 import Camera from "./camera";
 import Gallery from "./gallery";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const Tabs = TabNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
       tabBarLabel: 'Home',
+      tabBarIcon: () => <Icon name="home" size={30} />,
     },
   },
   Camera: {
     screen: Camera,
     navigationOptions: {
       tabBarLabel: 'Camera',
+      tabBarIcon: () => <Icon name="camera" size={24} />,
     },
   },
   Gallery: {
     screen: Gallery,
     navigationOptions: {
       tabBarLabel: 'Gallery',
+      tabBarIcon: () => <Icon name="picture-o" size={24} />,
     },
   }
 });
