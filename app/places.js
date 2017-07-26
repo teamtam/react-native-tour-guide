8ccee8468c32647958a2ff46de198df7e7610576
Benjamin Tam <ben@teamtam.com>
2017-07-26 23:40:23 +1000

    Add some lat/long data

---
+++ app/places.js
@@ -0,0 +1,14 @@
+var places = 
+  [
+    { "name": "Arts Centre Melbourne", "lat": -37.821453, "long": 144.968417, "image": "ArtsCentre.jpg" },
+    { "name": "Crown Casino", "lat": -37.823530, "long": 144.958095, "image": "CrownCasino.jpg" },
+    { "name": "Eureka Skydeck 88", "lat": -37.821638, "long": 144.964535, "image": "EurekaSkydeck.jpg" },
+    { "name": "Federation Square", "lat": -37.817979, "long": 144.969058, "image": "FederationSquare.jpg" },
+    { "name": "Melbourne Cricket Ground", "lat": -37.819967, "long": 144.983449, "image": "MCG.jpg" },
+    { "name": "Melbourne Zoo", "lat": -37.784135, "long": 144.951547, "image": "MelbourneZoo.jpg" },
+    { "name": "Old Melbourne Gaol", "lat": -37.807668, "long": 144.964897, "image": "OldMelbourneGaol.jpg" },
+    { "name": "Royal Botanic Gardens", "lat": -37.830369, "long": 144.979606, "image": "BotanicGardens.jpg" },
+    { "name": "Shrine of Remembrance", "lat": -37.830516, "long": 144.973432, "image": "ShrineOfRemembrance.jpg" },
+    { "name": "St Patrick's Cathedral", "lat": -37.810083, "long": 144.976417, "image": "StPatricks.jpg" },
+    { "name": "St Pauls's Cathedral", "lat": -37.817029, "long": 144.967687, "image": "StPauls.jpg" }
+  ]
